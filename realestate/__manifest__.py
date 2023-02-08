5e0fd2e08136194dc575530f99fe1b97558f37c0
Benoit Balza (BEBA) <beba@odoo.com>
2023-02-08 10:39:58 +0100

    realestate/

---
+++ realestate/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': "Real Estate",
+    'version': '14',
+    'depends': ['base','CRM'],
+    'author': "BEBA",
+    'category': 'Realestate',
+    'description': """
+    APP for real estate
+    """,
+    # data files always loaded at installation
+    'data': [
+        'views/realestate_view.xml',
+    ],
+}
