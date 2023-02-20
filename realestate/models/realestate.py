dcd9aa41d00607e8628504ef7cfd1a10c3132505
Benoit Balza (BEBA) <beba@odoo.com>
2023-02-20 11:04:45 +0100

    beba

--- realestate/models/realestate.py
+++ realestate/models/realestate.py
@@ -3,7 +3,7 @@ from odoo import models, fields
 class realestate(models.Model):
     _name = "real.estate"
     _description = "Real Estate"
-    name = fields.Char(required=True)
+    name = fields.Char(required=True,index=True)
     description = fields.Text()
     postcode = fields.Char()
     date_availability = fields.Date()
